457693578fe625d6726f044a7b212c7332d710ef
szewczykmira <szewczyk.mira@gmail.com>
2014-11-10 21:18:39 +0100

    Index page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'index/index'
+
   resources :comments
 
   resources :categories
@@ -12,7 +14,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'index#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
