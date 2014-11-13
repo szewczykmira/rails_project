b4707ddfdf29cc42d80ffd4be6ddc430369161fc
szewczykmira <szewczyk.mira@gmail.com>
2014-11-13 14:41:24 +0100

    Users view added

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141109223122) do
+ActiveRecord::Schema.define(version: 20141113132325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: true do |t|
     t.string   "name"
-    t.text   "dscr"
+    t.text     "dscr"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "comments", force: true do |t|
-    t.text   "content"
+    t.text     "content"
     t.integer  "user_id"
     t.integer  "game_id"
     t.datetime "created_at"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20141109223122) do
 
   create_table "games", force: true do |t|
     t.string   "name"
-    t.text   "dscr"
+    t.text     "dscr"
     t.date     "release_date"
     t.integer  "category_id"
     t.datetime "created_at"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 20141109223122) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "encrypted_password",     default: "abc", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
