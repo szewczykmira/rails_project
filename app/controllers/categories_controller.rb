93e01e5a89efaf6bdf14a5cb5a2df163928ca32c
szewczykmira <szewczyk.mira@gmail.com>
2014-11-23 18:40:06 +0100

    Some shit with categories fixed

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:show]
 
   respond_to :html
 
